8adb05296e08be4e9747e275d1b07b338fef539a
Braden McKallagat <braden.mckallagat@gmail.com>
2021-12-01 19:44:41 -0500

    initial commit

---
+++ frontend.js
@@ -0,0 +1,56 @@
+// button
+let calculateShipping = document.getElementById('calculate-shipping');
+
+calculateShipping.onclick = function () {
+    // text boxes
+    let ratePrice = document.getElementById('rate-price');
+    let totalPrice = document.getElementById('total-price');
+    let name = document.getElementById('name');
+    let street1 = document.getElementById('street1');
+    let street2 = document.getElementById('street2');
+    let city = document.getElementById('city');
+    let state = document.getElementById('state');
+    let zip = document.getElementById('zip');
+
+    // arrays?
+    let shippingMethods = document.getElementById('shipping-methods');
+    let orders = document.getElementById('orders');
+
+    let request = new XMLHttpRequest()
+
+    // Open a new connection, using the GET request on the URL endpoint
+    request.open('POST', 'https://us-central1-nots-backend-dev.cloudfunctions.net/widgets/shipping/estimate');
+    request.setRequestHeader('Content-Type', 'application/json');
+    request.send(JSON.stringify({
+        weight: 1,
+        shipTo: {
+            name: name.innerText,
+            addressLine1: street1.innerText,
+            addressLine2: street2.innerText,
+            city: city.innerText,
+            state: state.innerText,
+            zipCode: zip.innerText,
+        },
+    }));
+
+    request.onload = () => {
+        if (this.status === 200) {
+            let data = JSON.parse(this.response)
+
+            // Set the updated address
+            street1.innerText = data.address.addressLine1;
+            street2.innerText = data.address.addressLine2;
+            city.innerText = data.address.city;
+            state.innerText = data.address.state;
+            zip.innerText = data.address.zipCode;
+
+            // Set the shipping rates
+
+        } else {
+            console.log(`${this.status} - ${this.response}`);
+        }
+    }
+
+    // Send request
+    request.send();
+}

911c95fb49277063aa634b9de2ba7e59fcd7dea4
Braden McKallagat <braden.mckallagat@gmail.com>
2021-12-02 20:18:46 -0500

    try that

--- frontend.js
+++ frontend.js
@@ -41,7 +41,7 @@ calculateShipping.onclick = function() {
             // Set the shipping rates
             shippingMethods.innerHTML = '';
             data.rates.map(rate => {
-                let wrapper = createElement('div', {class: 'shipping-method-container'});
+                let wrapper = createElement('div', {'class': 'shipping-method-container'});
                 wrapper.appendChild(createElement('input', {type: 'radio', id: rate.rateId}));
                 let label = createElement('label', {for: rate.rateId});
                 label.innerText = `${rate.provider} - $${rate.cost} - ~${rate.estimatedDays} days`;
